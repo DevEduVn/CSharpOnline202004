c1567a6855abd20c7d4a6ea5e6074c42255fb41a
= <=>
2020-04-05 14:52:49 +0700

    Update Lesson01

---
+++ Lessons/Lesson01/Lesson01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Devmaster Academy");
+            Console.WriteLine("CSharp Training Courses Free at Devmater Academy");
+            Console.ReadKey();
+        }
+    }
+}
