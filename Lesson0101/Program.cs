e9acb3081d7430f7a8bb25c5e44ed56e3de73f5b
= <=>
2020-04-06 21:27:08 +0700

    UPdate lesson 01

---
+++ Lesson0101/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson0101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Biến, kiểu dữ liệu, khai báo...
+            //Cú pháp:
+            /*
+                Kieu_du_lieu  Ten_bien;
+                Kieu_du_lieu  Ten_Bien = Gia_Trị [,..] ;
+
+                Kiểu dữ liệu động
+                var ten_bien=giatri;
+
+             */
+            //Khai báo biến kiểu số nguyên
+            int x, y, z;
+            float a = 12.5f, b = 13.5f;
+            string Dia_Chi = "25 Vũ Ngọc Phan";
+            // Gán giá trị
+            x = 5;
+            y = 15;
+            z = x + y;
+
+            // In ra trị
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(Dia_Chi);
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, z);
+
+            Console.ReadKey();
+
+            /*
+             *  HẰNG
+              - Giá trị không đổi trong suốt quá trình tính toán
+              - Cú pháp:
+              const Kieu_du_lieu Ten_hang = Gia_tri;
+
+             */
+            const double PI = 3.1415;
+
+            Console.WriteLine(PI);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson0102/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson0102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Các toán tử
+            // Toán tử số học: +; -; *; /; %
+
+            // Toán tử so sánh: >; >=; <; <=; ==; !=
+
+            // Toán tử logic: && (and - và) ;  || (or - hoặc); !
+
+            // Toán tử gán: =; +=; -=; *=; /=; %=
+
+            // Toán tử tăng/ giảm: ++; --
+
+            //  Toán tử nối chuỗi: +
+
+            // Toán tử điều kiện: (Biểu thức điều kiện) ? <gt đúng> : <gt sai>;
+
+            //example
+            int x = 115, y = 255;
+            int z;
+            // Toán tử số học: +; -; *; /; %
+            z = x + y;
+            Console.WriteLine("{0} + {1} = {2}", x, y, z);
+
+            z = x - y;
+            Console.WriteLine("{0} - {1} = {2}", x, y, z);
+
+            z = x * y;
+            Console.WriteLine("{0} * {1} = {2}", x, y, z);
+
+            z = x / y;
+            Console.WriteLine("{0} / {1} = {2}", x, y, z);
+
+            z = x % y;
+            Console.WriteLine("{0} % {1} = {2}", x, y, z);
+
+            //So sánh
+
+            bool flag;
+            flag = x > y;
+            Console.WriteLine(flag);
+
+            flag = x < y;
+            Console.WriteLine(flag);
+
+            flag = (x >= y) && (x < y);
+            Console.WriteLine(flag);
+
+            flag = (x >= y) || (x < y);
+            Console.WriteLine(flag);
+
+            flag = !(x >= y);
+            Console.WriteLine(flag);
+
+            // gán:
+            x = 123;
+            Console.WriteLine("x++ = {0} ", x++);
+
+            Console.WriteLine("x++ = {0} ", x++);
+
+            y = 12;
+            Console.WriteLine("++y={0}", ++y);
+
+            z = 123;
+            z += 100; // z=z+100;
+
+        }
+    }
+}
